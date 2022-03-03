5ee871657247ecdb2bf0ca813ba026b12d6b1628
Ahmad Abdalla <28486158+ahmadabdalla@users.noreply.github.com>
2022-03-03 12:22:06 +1100

    first test

---
+++ utilities/pipelines/sharedScripts/Set-GitHubEnvVariables.ps1
@@ -0,0 +1,30 @@
+﻿<#
+.SYNOPSIS
+Recieves an Input Object of Key/Value pairs and sets them as environment variables on the host
+
+.DESCRIPTION
+Recieves an Input Object of Key/Value pairs and sets them as environment variables on the host
+
+.PARAMETER KeyValuePair
+Mandatory. An Hashtable that contains the Key and Value that are being outputted as environment variables (Key=Value)
+
+.EXAMPLE
+ss
+
+#>
+function Set-GitHubEnvVariable {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [hashtable] $KeyValuePair
+    )
+    $Keys = $KeyValuePair.Keys
+    foreach ($Key in $Keys) {
+        Write-Host "$Key=$($KeyValuePair[$Key])"
+        if (Test-Path $Env:GITHUB_ENV -ErrorAction SilentlyContinue) {
+            Write-Host 'Creating Environment Variables'
+            Write-Output "$Key=$($KeyValuePair[$Key])" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
+        }
+        #[System.Environment]::SetEnvironmentVariable($Key,$KeyValuePair[$Key])
+    }
+}
